64fe49aed25f79caaa28a09a57aecc1ed12f7fc6
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-07-11 23:21:56 -0300

    Initial Commit

---
+++ horoscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  horoscope
+//
+//  Created by Wagner Barboza Goulart on 11/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
